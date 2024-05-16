6eab208abaa6b19ca6ad86331804f3b26b27b844
SergEnergy <kis8883244@gmail.com>
2024-05-16 21:02:25 +0300

    feat: add route paths

--- src/route/paths.js
+++ src/route/paths.js
@@ -1,3 +1,9 @@
 export const APP_PATHS = {
-  //
+  BASE: '/',
+  MAIN_PAGE: '/main',
+  SIGN_UP: '/signup',
+  SIGN_IN: '/signin',
+  FAVORITES: '/favorites',
+  HISTORY: '/history',
+  NOT_FOUND: 'nor-found',
 };

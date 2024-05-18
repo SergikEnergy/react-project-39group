5bf8f2aba3d7b613263363aa581d15545bdcf87c
efedra <efedra-00@mail.ru>
2024-05-18 19:27:26 +0300

    refactor structure header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,19 @@
 import { Logo } from "./Logo";
+import { useState } from "react";
+import {NavNoAuthorize} from './navNoAuthorize'
+import {NavAuthorize} from "./navAuthorize";
 
 import { StyledHeader } from "./styled/StyledHeader";
 import { StyledContainerButtons } from "./styled/StyledHeader";
 
-import { Button } from "@mui/material";
-
-import { APP_PATHS } from "../../route/paths";
-import { Link } from "react-router-dom";
-import { useState } from "react";
-
 export const Header = () => {
+  // Ждем контекста, потом меняем на useContext
   const [isAuthorize, setAuthorize] = useState(null);
-
   return (
     <StyledHeader>
       <Logo />
       <StyledContainerButtons>
-        {/*Может надо прокидывать renderProps?*/}
-        {/*Если не авторизированный пользователь*/}
-        {!isAuthorize && <Link to={APP_PATHS.SIGN_IN}>
-          <Button variant="outlined" onClick={() => setAuthorize(1)}>Вход</Button>
-        </Link>}
-        {!isAuthorize && <Link to={APP_PATHS.SIGN_UP}>
-          <Button variant="outlined" onClick={() => setAuthorize(1)}>Регистрация</Button>
-        </Link>}
-        {/*Если авторизированный*/}
-        {isAuthorize && <Link to={APP_PATHS.HISTORY}>
-          <Button variant="outlined">История</Button>
-        </Link>}
-        {isAuthorize && <Link to={APP_PATHS.FAVORITES}>
-          <Button variant="outlined">Избранное</Button>
-        </Link>}
-        {isAuthorize && <Link to={APP_PATHS.MAIN_PAGE}>
-          <Button variant="outlined" onClick={() => setAuthorize(null)}>Выход</Button>
-        </Link>}
+        {isAuthorize ? <NavAuthorize logOut={()=>setAuthorize(null)}/> : <NavNoAuthorize logIn={()=>setAuthorize(1)}/> }
       </StyledContainerButtons>
     </StyledHeader>);
 };

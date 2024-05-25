76cdedb0dc9bfe428b473aeab8f3ac058817ce18
SergEnergy <kis8883244@gmail.com>
2024-05-25 16:36:12 +0300

    fix: hotfix styles and merge main and card

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -8,14 +8,20 @@ export const SearchBar = () => {
   const products = useProductsSelector();
   const options = products.suggestions.map((item) => item.label);
 
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <Autocomplete
       options={options}
       renderInput={(params) => (
         <TextField
+          onChange={handleInputChange}
           {...params}
           label="Search"
           variant="outlined"
+          type="text"
           style={{ display: 'flex', justifyContent: 'center', margin: '0 auto', maxWidth: '500px' }}
         />
       )}

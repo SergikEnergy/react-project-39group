d13608f39c692a5f3e0108d9500892ec8dbb9eb3
Kostya-x-pro <Kostya-voronkov1992@rambler.ru>
2024-05-25 19:13:54 +0300

    chang view page notFound

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,35 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Card, CardContent, Typography } from '@mui/material';
-
-import Img_404 from '../../images/404.jpg';
-
 import './NotFoundPage.css';
 
 export const NotFoundPage = () => (
-  <div className="wrapper__not-found">
-    <Typography
-      variant="h4"
-      style={{ zIndex: 3, color: 'rgb(36, 1, 161)' }}
-      component="h3"
-      align="center">
-      Извините, страница не найдена!
-    </Typography>
-    <Button
-      variant="contained"
-      className="button__not-found"
-      component={Link}
-      to="/main">
-      На Главную
-    </Button>
-    <Card className="card__not-found">
-      <CardContent>
-        <img
-          className="image__not-found"
-          src={Img_404}
-          alt="Not Found"
-        />
-      </CardContent>
-    </Card>
-  </div>
+    <div class="error_wrapper">
+      <div class="error_botImg"></div>
+      <div class="error">
+        <h1>404 Страница не найдена</h1>
+        <p>
+          Проверьте правильность ввода адреса, вернитесь на предыдущую страницу или попробуйте
+          воспользоваться поиском по сайту, чтобы найти что-то конкретное.
+        </p>
+        <button>Вернуться на главную</button>
+      </div>
+    </div>
 );
